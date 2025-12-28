6bd59662be8084ba073277b5f18883615ac5245c
Mouad <mouadananouch7@gmail.com>
2025-12-28 17:59:45 +0100

    Add Search

---
+++ src/app/tenant/search/search-date/search-date.component.spec.ts
@@ -0,0 +1,24 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {SearchDateComponent} from './search-date.component';
+
+describe('SearchDateComponent', () => {
+  let component: SearchDateComponent;
+  let fixture: ComponentFixture<SearchDateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SearchDateComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SearchDateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    // expect(component).toBeTruthy();
+    (expect(component) as unknown as jasmine.Matchers<any>).toBeTruthy();
+  });
+});
