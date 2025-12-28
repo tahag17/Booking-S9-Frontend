8510ee9f6c5cd525b4154ac2febb65cf133d8c3c
TahaGh <taha.ghailan17@gmail.com>
2025-12-28 11:35:13 +0100

    change production backendurl (rely on proxy only)

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL: 'https://booking-backend-295607ecab74.herokuapp.com/api',
-  BACKEND_URL: 'https://booking-backend-295607ecab74.herokuapp.com',
+  API_URL: '/api',
+  // BACKEND_URL: 'https://booking-backend-295607ecab74.herokuapp.com',
   production: true,
 };
